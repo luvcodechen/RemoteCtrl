94a0929ee995f37e2f6f53f3067cb431534ec616
luvcodechen <chen_ney@qq.com>
2024-12-30 10:10:29 +0800

    鼠标消息处理

--- RemoteCtrl/ServerSocket.h
+++ RemoteCtrl/ServerSocket.h
@@ -142,6 +142,22 @@ public:
 };
 #pragma pack(pop)
 
+
+typedef struct MouseEvent
+{
+	MouseEvent()
+	{
+		nAction = 0;
+		nButton = -1;
+		ptXY.x = 0;
+		ptXY.y = 0;
+	}
+
+	WORD nAction; //点击，移动，双击
+	WORD nButton; //左键，右键、中键
+	POINT ptXY; //鼠标坐标
+} MOUSEEV, *PMOUSEEV;
+
 class CServerSocket
 {
 public:
@@ -243,6 +259,16 @@ public:
 		if ((m_packet.sCmd >= 2) && (m_packet.sCmd <= 4))
 		{
 			strPath = m_packet.strData;
+			return true;
+		}
+		return false;
+	}
+
+	bool GetMouseEvent(MOUSEEV& mouse)
+	{
+		if (m_packet.sCmd == 5)
+		{
+			memcpy(&mouse, m_packet.strData.c_str(), sizeof(MOUSEEV)); //获取鼠标事件
 			return true;
 		}
 		return false;
