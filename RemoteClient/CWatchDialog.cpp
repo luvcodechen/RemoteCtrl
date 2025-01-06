31e4e47012a73282057ba8535e0f8b03a5d7a052
luvcodechen <chen_ney@qq.com>
2025-01-06 11:13:31 +0800

    设计鼠标远程控制的结构，实现本地鼠标信息获取封装和发送

--- RemoteClient/CWatchDialog.cpp
+++ RemoteClient/CWatchDialog.cpp
@@ -29,11 +29,28 @@ void CWatchDialog::DoDataExchange(CDataExchange* pDX)
 
 BEGIN_MESSAGE_MAP(CWatchDialog, CDialog)
 	ON_WM_TIMER()
+	ON_WM_LBUTTONDBLCLK()
+	ON_WM_LBUTTONDOWN()
+	ON_WM_LBUTTONUP()
+	ON_WM_RBUTTONDBLCLK()
+	ON_WM_RBUTTONDOWN()
+	ON_WM_RBUTTONUP()
+	ON_WM_MOUSEMOVE()
+	ON_STN_CLICKED(IDC_WATCH, &CWatchDialog::OnStnClickedWatch)
 END_MESSAGE_MAP()
 
 
 // CWatchDialog 消息处理程序
 
+
+CPoint CWatchDialog::UserPoint2RemoteScreenPoint(CPoint& point)
+{
+	//800 450
+	CRect clientRect;
+	ScreenToClient(&point); //转换为相对坐标
+	m_picture.GetWindowRect(clientRect); //获取控件的坐标
+	return CPoint(point.x * 3840 / clientRect.Width(), point.y * 2160 / clientRect.Height());
+}
 
 BOOL CWatchDialog::OnInitDialog()
 {
@@ -65,4 +82,126 @@ void CWatchDialog::OnTimer(UINT_PTR nIDEvent)
 		}
 	}
 	CDialog::OnTimer(nIDEvent);
+}
+
+
+void CWatchDialog::OnLButtonDblClk(UINT nFlags, CPoint point)
+{
+	CPoint remotePoint = UserPoint2RemoteScreenPoint(point);
+	//封装鼠标事件
+	MOUSEEV event;
+	event.ptXY = remotePoint; //鼠标坐标
+	event.nButton = 0; //左键
+	event.nAction = 2; //双击
+	CClientSocket* pClient = CClientSocket::GetInstance();
+	CPacket packet(5, (BYTE*)&event, sizeof(event));
+	pClient->Send(packet);
+	CDialog::OnLButtonDblClk(nFlags, point);
+}
+
+
+void CWatchDialog::OnLButtonDown(UINT nFlags, CPoint point)
+{
+	CDialog::OnLButtonDown(nFlags, point);
+}
+
+
+void CWatchDialog::OnLButtonUp(UINT nFlags, CPoint point)
+{
+	// CPoint remotePoint = UserPoint2RemoteScreenPoint(point);
+	// //封装鼠标事件
+	// MOUSEEV event;
+	// event.ptXY = remotePoint; //鼠标坐标
+	// event.nButton = 0; //左键
+	// event.nAction = 4; //弹起
+	// CClientSocket* pClient = CClientSocket::GetInstance();
+	// CPacket packet(5, (BYTE*)&event, sizeof(event));
+	// pClient->Send(packet);
+
+
+	CDialog::OnLButtonUp(nFlags, point);
+}
+
+
+void CWatchDialog::OnRButtonDblClk(UINT nFlags, CPoint point)
+{
+	CPoint remotePoint = UserPoint2RemoteScreenPoint(point);
+	//封装鼠标事件
+	MOUSEEV event;
+	event.ptXY = remotePoint; //鼠标坐标
+	event.nButton = 2; //右键
+	event.nAction = 2; //双击
+	CClientSocket* pClient = CClientSocket::GetInstance();
+	CPacket packet(5, (BYTE*)&event, sizeof(event));
+	pClient->Send(packet);
+
+
+	CDialog::OnRButtonDblClk(nFlags, point);
+}
+
+
+void CWatchDialog::OnRButtonDown(UINT nFlags, CPoint point)
+{
+	CPoint remotePoint = UserPoint2RemoteScreenPoint(point);
+	//封装鼠标事件
+	MOUSEEV event;
+	event.ptXY = remotePoint; //鼠标坐标
+	event.nButton = 2; //右键
+	event.nAction = 3; //按下
+	CClientSocket* pClient = CClientSocket::GetInstance();
+	CPacket packet(5, (BYTE*)&event, sizeof(event));
+	pClient->Send(packet);
+
+
+	CDialog::OnRButtonDown(nFlags, point);
+}
+
+
+void CWatchDialog::OnRButtonUp(UINT nFlags, CPoint point)
+{
+	CPoint remotePoint = UserPoint2RemoteScreenPoint(point);
+	//封装鼠标事件
+	MOUSEEV event;
+	event.ptXY = remotePoint; //鼠标坐标
+	event.nButton = 2; //右键
+	event.nAction = 4; //弹起
+	CClientSocket* pClient = CClientSocket::GetInstance();
+	CPacket packet(5, (BYTE*)&event, sizeof(event));
+	pClient->Send(packet);
+
+
+	CDialog::OnRButtonUp(nFlags, point);
+}
+
+
+void CWatchDialog::OnMouseMove(UINT nFlags, CPoint point)
+{
+	CPoint remotePoint = UserPoint2RemoteScreenPoint(point);
+	//封装鼠标事件
+	MOUSEEV event;
+	event.ptXY = remotePoint; //鼠标坐标
+	event.nButton = 0; //左键
+	event.nAction = 1; //移动
+	CClientSocket* pClient = CClientSocket::GetInstance();
+	CPacket packet(5, (BYTE*)&event, sizeof(event));
+	pClient->Send(packet);
+
+
+	CDialog::OnMouseMove(nFlags, point);
+}
+
+
+void CWatchDialog::OnStnClickedWatch()
+{
+	CPoint point;
+	GetCursorPos(&point); //获取鼠标坐标
+	CPoint remotePoint = UserPoint2RemoteScreenPoint(point);
+	//封装鼠标事件
+	MOUSEEV event;
+	event.ptXY = remotePoint; //鼠标坐标
+	event.nButton = 0; //左键
+	event.nAction = 3; //按下
+	CClientSocket* pClient = CClientSocket::GetInstance();
+	CPacket packet(5, (BYTE*)&event, sizeof(event));
+	pClient->Send(packet);
 }
